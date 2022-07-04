a4124b2fe74f0f50bd8e6d601172a428ddddfc7f
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-07-04 15:25:08 +1000

    backend setup, utilising mongoose to connect with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,52 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import User from './models/User.js';
 
 const app = express();
 
+const main = async () => {
+    await mongoose.connect("mongodb+srv://tomcsesoc:JuAsCcaPLvQrV7Tb@cluster0.7caoyfw.mongodb.net/?retryWrites=true&w=majority");
+}
+main().catch((err) => {console.log(err)});
+
+const db = mongoose.connection;
+db.on('error', err=> console.log(err));
+db.once('open', () => console.log('Connected to database'));
+
 app.use(express.json());
 
+app.post('/register', async (req, res) => {
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        age: req.body.age,
+        username: req.body.username,
+        password: req.body.password,
+        description: req.body.description
+    })
 
-app.get('/', (req, res) => {
-    res.status(200).json({ msg: "Inside get request" });
+    try {
+        const newUser = await user.save();
+        res.status(200).json(newUser)
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
 })
 
-app.use(express.json());
-
-app.post('/register', (req, res) => {
-    console.log(req.body.Name);
-    res.status(200).json(req.body);
+app.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        if (!user) {
+            res.status(404).json({message: 'User not found'});
+        } else if (user.password !== req.body.password) {
+            res.status(403).json({message: 'Password incorrect'});
+        } else {
+            const {password, ...others} = user._doc;
+            res.status(200).json(others);
+        }
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
 })
 
 app.listen(8000, () => { console.log('Server is running on port 8000!') });
