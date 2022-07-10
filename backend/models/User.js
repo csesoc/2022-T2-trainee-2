2deb0672fbe57e09a57457f97777ab003db744db
Ahmed Ibrahim <106638732+Hetchy@users.noreply.github.com>
2022-07-10 20:31:55 +1000

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,31 +2,27 @@ import mongoose from 'mongoose';
 
 const UserSchema = mongoose.Schema(
     {
-        firstName: {
-            type: String, 
-            required: true
-        },
-        lastName: {
-            type: String, 
-            required: true
-        },
-        age: {
-            type: Number, 
-            required: true
-        }, 
-        username: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
+        email: {type: String, required: true},
+        username: {type: String, required: true, unique: true, trim: true},
+        password: {type: String, required: true},
+        firstName: {type: String, required: true, trim: true},
+        lastName: {type: String, required: true, trim: true},
+        age: {type: Number, required: true, min: 16},
+        description: {type: String, required: true},
+        showLocation: {type: Boolean, default: false},
+        dayAvailabilities: [Boolean], // array of 7
+        tags: [String], // array of 8 max
+        
+        matches: [{
+            matchedWithUsername: String,
+            matchStatus: {type: Boolean, default: false} // pending == false, matched == true
+        }],
+        
+        socials: [{
+            platform: String,
+            username: String
+        }], 
+        showSocials: {type: Boolean, default: false}
     }
 );
 
