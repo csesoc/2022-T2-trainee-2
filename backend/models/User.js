3e66ce4edc5c9aa52c7b2084866e7a26a32554ea
Ahmed Ibrahim <106638732+Hetchy@users.noreply.github.com>
2022-07-15 18:20:22 +1000

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose';
 
 const UserSchema = mongoose.Schema(
     {
-        email: {type: String, required: true},
-        username: {type: String, required: true, unique: true, trim: true},
-        password: {type: String, required: true},
-        firstName: {type: String, required: true, trim: true},
-        lastName: {type: String, required: true, trim: true},
-        age: {type: Number, required: true, min: 16},
+        Name: {type: String, required: true},
+        age: {type: Number, min: 16},
         description: {type: String, required: true},
+        interests: String,
         showLocation: {type: Boolean, default: false},
         dayAvailabilities: [Boolean], // array of 7
         tags: [String], // array of 8 max
