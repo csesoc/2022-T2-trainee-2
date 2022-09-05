5a169058bc0f721623ebb3e2f451d6bd2982d54d
wanning <z5371162@unsw.edu.au>
2022-09-05 23:11:51 +1000

    fixed favicon

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from './LandingPage.module.css';
 import Header from '../../components/Header/Header';
+import image from '../../images/loginimage.png';
 // Login
 
 export default function LandingPage() {
+  function onSubmit() {
+    fetch('localhost:8000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: {
+        username: 'bob',
+        password: 'bob123',
+      },
+    });
+  }
   return (
     <div className={styles.MainBlock}>
-      <Header title='Find your people on Groupr!' />
-      <form>
+      <Header title='swipe and find your new tribe!' />
+      <img src={image} style={{ width: '80%', margins: 'auto' }}></img>
+      <form onSubmit={onSubmit}>
         <input className={styles.text} type='text' placeholder='username' />
         <br />
         <input className={styles.text} type='password' placeholder='password' />
